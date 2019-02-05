7cd041c8e58b046f8dbf1820bf0bc410f474e5f9
Tan Gemicioglu <tangem1@hotmail.com>
2019-02-05 19:33:10 +0300

    Fill robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.SPI;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -14,13 +16,34 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+    //CAN
+    public static final int driveFrontLeftMotor = 0;
+    public static final int driveRearLeftMotor = 0;
+    public static final int driveFrontRightMotor = 0;
+    public static final int driveRearRightMotor = 0;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+    public static final int elevatorLeftMotor = 0;
+    public static final int elevatorRightMotor = 0;
+
+    public static final int cargoIntakeMotor = 0;
+
+    public static final int cargoIntakeWristMotor = 0;
+
+    public static final int hatchWristMotor = 0;
+    //DIO
+    public static final int driveLeftEncoderA = 0;
+    public static final int driveLeftEncoderB = 0;
+    public static final int driveRightEncoderA = 0;
+    public static final int driveRightEncoderB = 0;
+
+    public static final int elevatorEncoderA = 0;
+    public static final int elevatorEncoderB = 0;
+
+    public static final int cargoIntakeWristEncoderA = 0;
+    public static final int cargoIntakeWristEncoderB = 0;
+    public static final int cargoIntakeWristBottomSwitch = 0;
+
+    public static final int hatchWristBottomSwitch = 0;
+    //NavX
+    public static final SPI.Port navx = SPI.Port.kMXP;
 }
