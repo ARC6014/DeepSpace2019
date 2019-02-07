32e790a2ac23ddb0c7cbd967afb0c46045f89270
guvkay20 <guvkay.20@robcol.k12.tr>
2019-02-07 17:09:04 +0300

    Pathfinding subsystem created, empty.

--- src/main/java/frc/robot/subsystems/Pathfinding.java
+++ src/main/java/frc/robot/subsystems/Pathfinding.java
@@ -8,6 +8,12 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.TankModifier;
+
+
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
@@ -17,8 +23,27 @@ public class Pathfinding extends Subsystem {
     // here. Call these from Commands.
 
     @Override
-    public void initDefaultCommand() {
+    public void pathfinding() {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
     }
+
+    public void pathfinding(double robotFrontalWidth) {
+        this.robotFrontalWidth = robotFrontalWidth;
+    }
+
+    // takes angle as reference from present forward direction, in cw direction, in degrees
+    public WayPoint[] newPath(double distance, double angle, int destinationID = 0) {
+        Waypoint[] waypoints = new Waypoint[] {
+                new Waypoint(0, 0, 0);
+                new Waypoint(sin(Pathfinder.d2r(angle)) * distance, cos(Pathfinder.d2r(angle)) * distance, Pathfinder.d2r(angle));
+        };
+
+        if (destinationID != 0) {
+            // Implement obstacles if needed
+        }
+
+    }
+
+    private double robotFrontalWidth;
 }
