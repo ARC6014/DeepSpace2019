40330d3aa1f507e4f917e62c80e2c4bd02596f9b
VVanguard <bkaymakcioglu@gmail.com>
2019-02-09 20:49:34 +0300

    add Drive PID Subsystem

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -7,18 +7,53 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import com.kauailabs.navx.frc.AHRS;
+import frc.robot.RobotMap;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class Drive extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+public class Drive extends PIDSubsystem {
+  private double outPID = 0;
+  AHRS mxp = new AHRS(SPI.Port.kMXP);
+
+  VictorSPX driveRearRightMotor = new VictorSPX(RobotMap.driveRearRightMotor);
+  VictorSPX driveFrontRightMotor = new VictorSPX(RobotMap.driveFrontRightMotor);
+  VictorSPX driveRearLeftMotor = new VictorSPX(RobotMap.driveRearLeftMotor);
+  VictorSPX driveFrontLeftMotor = new VictorSPX(RobotMap.driveFrontLeftMotor);
+
+  Encoder driveRightEncoder = new Encoder(RobotMap.driveRightEncoderA, RobotMap.driveRightEncoderB, false, Encoder.EncodingType.k4X);
+  Encoder driveLeftEncoder = new Encoder(RobotMap.driveLeftEncoderA, RobotMap.driveLeftEncoderB, false, Encoder.EncodingType.k4X);
+
+  public Drive() {
+    super(0,0,0);
+    setAbsoluteTolerance(5);
+    getPIDController().setInputRange(180.0,-180.0);
+    getPIDController().setOutputRange(-1,1);
+    getPIDController().setContinuous(true);
+  }
 
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  public void initDefaultCommand() { }
+
+  public double getHeading() {
+    return mxp.getYaw();
+  }
+  public void reset() {
+    mxp.reset();
+  }
+
+  @Override
+  public double returnPIDInput() {
+   return getHeading();
+  }
+
+  @Override
+  public void usePIDOutput(double output) {
+    this.outPID = output;
   }
 }
