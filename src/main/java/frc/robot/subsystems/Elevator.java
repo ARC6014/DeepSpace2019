ae826718da4c740bc23ee62a5ce81c944bc04e2a
VVanguard <bkaymakcioglu@gmail.com>
2019-02-09 18:54:54 +0300

    add Elevator PID Subsystem

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,18 +7,56 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class Elevator extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+public class Elevator extends PIDSubsystem {
+  private double outPID = 0;
+  private final double chainPitch = 0.25;
+  private final double driveTeeth = 22;
+  private final double drivenTeeth = 22;
+  private final double outputRatio = 2.5;
+  private final double encoderPPR = 2048;
+
+  Encoder elevatorEncoder = new Encoder(RobotMap.elevatorEncoderA, RobotMap.elevatorEncoderB, false, Encoder.EncodingType.k4X);
+  VictorSPX elevatorRightMotor = new VictorSPX(RobotMap.elevatorRightMotor);
+  VictorSPX elevatorLeftMotor = new VictorSPX(RobotMap.elevatorLeftMotor);
+
+
+  public Elevator() {
+    super(0,0,0);
+    setAbsoluteTolerance(1);
+    getPIDController().setInputRange(0,100);
+    getPIDController().setOutputRange(-1,1);
+
+    elevatorLeftMotor.follow(elevatorRightMotor);
+  }
 
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  public void initDefaultCommand() { }
+
+  @Override
+  public double returnPIDInput() {
+    return encoderValue_cm();
   }
+
+  @Override
+  public void usePIDOutput(double output) {
+    this.outPID = output;
+  }
+
+  public double encoderValue_cm() {
+    return (elevatorEncoder.get()/encoderPPR) / outputRatio * (drivenTeeth/driveTeeth) * chainPitch * 2.54;
+  }
+
+  public void reset() {
+    elevatorEncoder.reset();
+  }
+
+
 }
