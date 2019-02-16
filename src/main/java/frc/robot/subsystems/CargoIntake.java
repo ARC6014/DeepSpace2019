e3c4549b49fe2a470213ac143937fff897e9b792
Defne ertuğrul <ertdef.22@robcol.k12.tr>
2019-02-16 18:40:42 +0000

    Update CargoIntake.java

--- src/main/java/frc/robot/subsystems/CargoIntake.java
+++ src/main/java/frc/robot/subsystems/CargoIntake.java
@@ -13,12 +13,13 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * An example subsystem.  You can replace me with your own Subsystem.
  */
 public class CargoIntake extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+    
+
+  
+  VictorSPX cargoIntakeMotor = new VictorSPX(RobotMap.cargoIntakeMotor);
 
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+
   }
 }
