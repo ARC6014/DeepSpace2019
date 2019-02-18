270d7947ba06796d60923c44f06f67952a528395
VVanguard <bkaymakcioglu@gmail.com>
2019-02-19 00:41:49 +0300

    Revert "Update CargoIntakeWrist.java"
    This reverts commit 054da8ba4a336a374e6f70543a1ed4116451e368.

--- src/main/java/frc/robot/subsystems/CargoIntakeWrist.java
+++ src/main/java/frc/robot/subsystems/CargoIntakeWrist.java
@@ -12,48 +12,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class CargoIntakeWrist extends PIDSubsystem {
-    double outPID = 0;
-    VictorSPX cargoIntakeWristLeftMotor = new VictorSPX(RobotMap.cargoIntakeWristLeftMotor);
-    VictorSPX cargoIntakeWristRightMotor = new VictorSPX(RobotMap.cargoIntakeWristRightMotor);
-    Encoder cargoIntakeWristEncoder = new Encoder(RobotMap.cargoIntakeWristEncoderA, RobotMap.cargoIntakeWristEncoderB, false, Encoder.EncodingType.k4X);
-    DigitalInput cargoIntakeWristBottomSwitch = new DigitalInput(RobotMap.cargoIntakeWristBottomSwitch);
-    
-    
-    
-    public CargoIntakeWrist() {
-    super(0,0,0);
-    setAbsoluteTolerance(1);
-    getPIDController().setInputRange(0,maxWristAngle);
-    getPIDController().setOutputRange(-1,1);
-    
-    public void initDefaultCommand() {
-  }
-
-    public void periodic() {
-    SmartDashboard.putNumber("WristAngle", getWristAngle());
-    
-  }
- public void resetEncoder() {
-    cargoIntakeWristEncoder.reset();
-  }
-
-  public double getEncoderRev() {
-    return cargoIntakeWristEncoder.get() / (double)encoderCPR;
-  }
-  public double getWristAngle() {
-  }
-  public void PIDRotate() {
-    setWristSpeed(outPID);
-  }
-
-
-
-
-  }
-
-    
-
+public class CargoIntakeWrist extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
