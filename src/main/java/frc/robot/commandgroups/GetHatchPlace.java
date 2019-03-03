b47999d0894de4c72c9d0c359b100889d01887c3
Bora Kaymakcioglu <kaybor.22@robcol.k12.tr>
2019-03-03 20:22:13 +0300

    configure controls

--- src/main/java/frc/robot/commandgroups/GetHatchPlace.java
+++ src/main/java/frc/robot/commandgroups/GetHatchPlace.java
@@ -2,6 +2,7 @@ package frc.robot.commandgroups;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
+import frc.robot.commands.DropHatch;
 import frc.robot.commands.LiftElevator;
 import frc.robot.commands.LiftHatch;
 import frc.robot.commands.TimedDrive;
@@ -9,10 +10,10 @@ import frc.robot.commands.TimedDrive;
 public class GetHatchPlace extends CommandGroup {
 
     public GetHatchPlace() {
-        if (Robot.elevator.elevatorHeightCm() + 20 < Robot.elevator.maxHeight) {
-            addSequential(new LiftHatch());
+        if (Robot.elevator.elevatorHeightCm() - 10 > Robot.elevator.baseToIntakeHeight) {
+            addSequential(new DropHatch());
             addSequential(new TimedDrive(0, -0.8,0.3));
-            addSequential(new LiftElevator(Robot.elevator.getSetpoint() - 15));
+            addSequential(new LiftElevator(Robot.elevator.getSetpoint() + 10));
         }
     }
 }
