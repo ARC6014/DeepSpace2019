550cd2f3847d47f744633d0edb658fae172ab4be
guvkay20 <guvkay.20@robcol.k12.tr>
2019-04-18 00:31:07 +0300

    Autonomous calibration use moved to command

---
+++ src/main/java/frc/robot/commands/Reader.java
@@ -0,0 +1,69 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Reader extends Command {
+    private ArrayList<ArrayList<Double>> motorValues = new ArrayList<ArrayList<Double>>();
+    private int i;
+
+    public Reader() {}
+
+    @Override
+    public void initialize() {
+        requires(Robot.elevator);
+        requires(Robot.drive);
+        requires(Robot.cargoIntake);
+        requires(Robot.cargoIntakeWrist);
+
+        i = 0;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("/home/lvuser/deploy/testData.txt"));
+            for(int i = 0; i < 6; i++) {
+                String line = reader.readLine();
+                int lastComma = -1;
+                for(int j = 2; j < line.length(); j++) {
+                    if (line.substring(j, j+1) == "," || line.substring(j, j+1) == "]") {
+                        double number = Double.parseDouble(line.substring(lastComma + 2, j));
+                        lastComma = j;
+                        motorValues.get(i).add(number);
+                    }
+                }
+            }
+
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Override
+    public void execute() {
+        Robot.cargoIntakeWrist.setMotorDirect(motorValues.get(1).get(i));
+        Robot.drive.setLeftMotorDirect(motorValues.get(2).get(i));
+        Robot.drive.setRightMotorDirect(motorValues.get(3).get(i));
+        Robot.cargoIntake.setMotorDirect(motorValues.get(4).get(i));
+        Robot.elevator.setMotorDirect(motorValues.get(5).get(i));
+        i++;
+    }
+
+    @Override
+    public void interrupted() {
+        end();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return i == motorValues.get(1).size();
+    }
+
+    @Override
+    public void end() {
+    }
+}
